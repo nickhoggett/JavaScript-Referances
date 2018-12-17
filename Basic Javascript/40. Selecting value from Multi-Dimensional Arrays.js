d9e37dbb466e001897da4795f439181c6609456b
nickhoggett <37800892+nickhoggett@users.noreply.github.com>
2018-12-17 09:25:50 +0000

    Add files via upload

---
+++ Basic Javascript/40. Selecting value from Multi-Dimensional Arrays.js
@@ -0,0 +1,19 @@
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1]; // returns 8.
+
+/* Example:
+
+var arr = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [[10,11,12], 13, 14]
+];
+arr[3]; // equals [[10,11,12], 13, 14]
+arr[3][0]; // equals [10,11,12]
+arr[3][0][1]; // equals 11
+
+*/
---
+++ Basic Javascript/45. Shopping List.js
@@ -0,0 +1,1 @@
+var myList = [["Bacon", 2],["Eggs", 2],["Grilled Tomatoes", 1],["Sasuage", 1],["Bean", 34]];
