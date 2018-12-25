945ef472d9822876926c559ad50f30e0334f7596
nickhoggett <37800892+nickhoggett@users.noreply.github.com>
2018-12-25 01:50:03 +0000

    Add files via upload

--- Basic Javascript/22. Escaping Literal Quotes Inside Strings.js
+++ Basic Javascript/22. Escaping Literal Quotes Inside Strings.js
@@ -1,1 +1,1 @@
-var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
---
+++ Basic Javascript/51. Reutrn Value from function with return command.js
@@ -0,0 +1,16 @@
+// Example
+function minusSeven(num) {
+    return num - 7;
+  }
+  
+  // Only change code below this line
+  
+  function timesFive(num2) {
+  
+    return num2 * 5;
+  
+  } 
+  
+  console.log(timesFive(5));
+  
+  console.log(minusSeven(10));
